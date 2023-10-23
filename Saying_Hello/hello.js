0e1ff77a86bbee244e2b47fedf409522594fac68
Danilo <danilomou01@gmail.com>
2023-10-23 01:35:49 -0300

    cleared exercise

---
+++ Saying_Hello/hello.js
@@ -0,0 +1,8 @@
+// for this exercise, it was required to keep input, concatenation and output separate.
+// prompt-sync is required to emulate the window object in node, so that the code runs in the terminal.
+const prompt = require('prompt-sync')();
+
+let username = prompt('What is your name? ')
+let greeting = (`Hello, ${username}, nice to meet you!`)
+
+console.log(greeting);
