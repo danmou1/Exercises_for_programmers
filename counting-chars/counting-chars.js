67c3a5d02818aabb92ef4cd958861dcf8924a76c
Danilo <danilomou01@gmail.com>
2023-10-25 19:07:19 -0300

    added null/empty string check

--- counting-chars/counting-chars.js
+++ counting-chars/counting-chars.js
@@ -3,9 +3,11 @@
 const prompt = require("prompt-sync")()
 
 let userInput = prompt('Insert the input string: ')
+while (userInput === null || userInput.length === 0){
+    userInput = prompt('Invalid string, please enter a proper string: ')
+}
 
 console.log(`${userInput} has ${userInput.length} characters.`);
-
 /*following is interchangeable code, in case the snippet above looks off
 
 let output = `${userInput} has ${userInput.lenght} characters.`
