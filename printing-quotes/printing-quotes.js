150999027a514f047ef1a6f07b855585756d7c5c
Danilo <danilomou01@gmail.com>
2023-11-01 19:47:03 -0300

    completed exercise

---
+++ printing-quotes/printing-quotes.js
@@ -0,0 +1,37 @@
+//the exercise requires the use of string concatenation, without interpolating
+//a single output statement should be used to produce the output.
+
+const { get } = require('node:http');
+const readline = require('node:readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let quote, author;
+
+function getQuote() {
+    rl.question('Insert a quote: ', (answer) => {
+        if (answer.trim()) {
+            quote = answer;
+            getAuthor();
+        } else {
+            console.log('Invalid quote.');
+            getQuote();
+        }
+        return quote;
+    });
+};
+function getAuthor(){
+    rl.question('Insert the name of the author: ', (answer) => {
+        if (answer.trim()) {
+            author = answer;
+            rl.close();
+            console.log(author + ' once said, ' + `"` + quote + `".`);
+        } else {
+            console.log('Invalid author name.');
+            getAuthor();
+        }
+    });
+}
+
+getQuote();
