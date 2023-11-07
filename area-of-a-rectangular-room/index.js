5da644258b0da2e081cb457ce4856eedce6f3e23
Danilo <danilomou01@gmail.com>
2023-11-07 10:23:50 -0300

    fix typo

--- area-of-a-rectangular-room/index.js
+++ area-of-a-rectangular-room/index.js
@@ -1,7 +1,6 @@
 //calculations must be separate from output
 //conversion value variable must be a constant
 //inputs must be numeric
-const { features } = require('process');
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
