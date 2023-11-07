0e0e332209d121ad6f6d390897e3a468ef425e62
Danilo <danilomou01@gmail.com>
2023-11-07 08:49:17 -0300

    typo fix

--- retirement-calculator.js/retirement-calculator.js
+++ retirement-calculator.js/retirement-calculator.js
@@ -1,7 +1,6 @@
 //get unix timestamp
 //if the program returns a negative number, assign the correct output
 
-const { resolve } = require('path');
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
