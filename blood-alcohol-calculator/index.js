8238e0136b521159e9c222d1c188055f39bc887e
Danilo <danilomou01@gmail.com>
2023-11-22 00:50:27 -0300

    incomplete

--- blood-alcohol-calculator/index.js
+++ blood-alcohol-calculator/index.js
@@ -8,3 +8,17 @@
  *  r = alcohol dritibution ratio
  *  H = number of hours since last drink 
  */
+
+const legalBAC = 0.08;
+
+function askQuestion(rl, prompt) {
+    return new Promise((resolve) => rl.question(prompt, resolve));
+};
+
+async function calcBAC() {
+    const weight = await askQuestion(rl, 'How much do you weight in pounds? ');
+    const gender = await askQuestion(rl, 'What is your gender? ');
+    const numDrinks = await askQuestion(rl, 'How many drinks did you have today? ');
+    const alcVolume = await askQuestion(rl, 'What was the alcohol volume of the drinks you consumed? ');
+    const time = await askQuestion(rl, 'How much time has passed after your last drink? ');
+}
