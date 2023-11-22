cb4bd9c9e0152c701a5392e826284a00bb7d0e49
Danilo <danilomou01@gmail.com>
2023-11-22 00:44:35 -0300

    init

---
+++ blood-alcohol-calculator/index.js
@@ -0,0 +1,10 @@
+/** TODO
+ * prompt weight, gender, number of drinks, alcohol volume of drinks, time since last drink
+ * 
+ * calculate BAC using this formula:
+ *  BAC = (A * 5.14 / W * r) - 0.015 * H
+ *  A = alcohol consumed in ounces
+ *  W = body weight in pounds
+ *  r = alcohol dritibution ratio
+ *  H = number of hours since last drink 
+ */
