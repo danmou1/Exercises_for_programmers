7228784aa83de617b49b0dd522daf5ecfba32664
Danilo <danilomou01@gmail.com>
2023-11-22 16:15:34 -0300

    complete

--- blood-alcohol-calculator/index.js
+++ blood-alcohol-calculator/index.js
@@ -1,24 +1,64 @@
-/** TODO
- * prompt weight, gender, number of drinks, alcohol volume of drinks, time since last drink
- * 
- * calculate BAC using this formula:
- *  BAC = (A * 5.14 / W * r) - 0.015 * H
- *  A = alcohol consumed in ounces
- *  W = body weight in pounds
- *  r = alcohol dritibution ratio
- *  H = number of hours since last drink 
- */
+// this code looks ugly
+const readline = require('readline');
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
 
-const legalBAC = 0.08;
+const LEGALBAC = 0.08;
 
 function askQuestion(rl, prompt) {
-    return new Promise((resolve) => rl.question(prompt, resolve));
+    return new Promise((resolve) => {
+        rl.question(prompt, (answer) => checkValue(answer, resolve));
+    });
+};
+
+function checkValue(answer, resolve) {
+    const value = parseFloat(answer);
+
+    if (isNaN(value) || value < 0) {
+        console.log('Invalid value, please enter a positive number or zero.')
+        askQuestion(rl, 'Try again: ').then(resolve);
+    } else {
+        resolve(answer)
+    }
+};
+
+function getGender(rl, prompt) {
+    return new Promise((resolve) => {
+        rl.question(prompt, (answer) => checkGender(answer, resolve));
+    })
+};
+
+function checkGender(answer, resolve) {
+    if (answer = 'm') {
+        resolve(0.73);
+    } else if (answer = 'f') {
+        resolve(0.66);
+    } else {
+        console.log('Invalid gender.');
+        getGender(rl, 'Try again. (m/f) ').then(resolve);
+    }
 };
 
 async function calcBAC() {
     const weight = await askQuestion(rl, 'How much do you weight in pounds? ');
-    const gender = await askQuestion(rl, 'What is your gender? ');
     const numDrinks = await askQuestion(rl, 'How many drinks did you have today? ');
-    const alcVolume = await askQuestion(rl, 'What was the alcohol volume of the drinks you consumed? ');
-    const time = await askQuestion(rl, 'How much time has passed after your last drink? ');
-}
+    const alcVolume = await askQuestion(rl, 'How many ounces of alcohol each drink you consumed had? ');
+    const time = await askQuestion(rl, 'How many hours have passed since your last drink? ');
+    const rate = await getGender(rl, 'What is your gender? (m/f) ');
+
+    let alcohol = numDrinks * alcVolume
+    const BAC = (alcohol * 5.14 / weight * rate) - 0.015 * time;
+
+    console.log(`Your BAC is ${BAC}.`)
+    if (BAC >= LEGALBAC) {
+        console.log('It is not legal for you to drive.');
+    } else {
+        console.log('It is legal for you to drive.');
+    }
+
+    rl.close();
+};
+
+calcBAC();
