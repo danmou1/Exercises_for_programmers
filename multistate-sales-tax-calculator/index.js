c846103ac049c9ddcf218371b67f64014e39b598
Danilo <danilomou01@gmail.com>
2023-11-23 18:58:52 -0300

    in progress

--- multistate-sales-tax-calculator/index.js
+++ multistate-sales-tax-calculator/index.js
@@ -8,29 +8,104 @@
  * print tax
  * print total
  */
+const readline = require('readline');
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
 
-function askQuestion(rl, prompt) {
+class County {
+    constructor(name, salesTax) {
+        this.name = name;
+        this.salesTax = salesTax;
+    };
+};
+
+class State {
+    constructor(abbreviation, name, salesTax, counties = []) {
+        this.abbreviation = abbreviation;
+        this.name = name;
+        this.salesTax = salesTax;
+        this.counties = counties;
+    };
+};
+
+const states = [
+    new State('IL', 'Illinois', 0.08),
+    new State('WI', 'Wisconsin', 0.08, [
+        new County('Eau Claire', 0.005),
+        new County('Dunn', 0.004)
+    ])
+];
+
+//prompts for a string, will keep prompting until it's a number.
+function promptNumber(rl, prompt) {
     return new Promise ((resolve) => {
-        rl.question(prompt, (answer) => checkValue(answer, resolve));
+        rl.question(prompt, (answer) => checkNumber(answer, resolve));
     })
 };
 
-function checkValue(answer,resolve) {
-    const unit = parseFloat(answer)
-    if (isNaN(unit) || unit <= 0){
+function checkNumber(answer, resolve) {
+    const number = parseFloat(answer);
+
+    if (isNaN(number) || number <= 0){
         console.log('Invaild value, please enter a number greater than 0');
-        askQuestion(rl, 'Try again: ').then(resolve);
+        promptNumber(rl, 'Try again: ').then(resolve);
     } else {
-        resolve(unit);
+        resolve(number);
     }
 };
 
-function getState(rl, prompt) {
+//prompts for state, if state exists checks if counties exist, if they do, prompt for them
+//if state doesn't exist, return 1
+//if state exists but county doesnt, return only state tax, which is 0.08
+function promptState(rl, prompt) {
     return new Promise ((resolve) => {
         rl.question(prompt, (answer) => checkState(answer, resolve));
-    })
+    });
 };
 
-function checkState(answer, resolve) {
-    
+async function checkState(answer, resolve) {
+    const lowercaseState = answer.toLowerCase();
+    const state = states.find(s => s.abbreviation.toLowerCase() === lowercaseState || s.name.toLowerCase() === lowercaseState);
+
+    if (!state) {
+        resolve(1);
+    }
+
+    //checking if the state contains a county
+    if(state.counties && state.counties.length > 0) {
+        await promptCounty(rl, state, 'From which county are you from? ');
+    } else {
+        resolve(state.salesTax);
+    }
 }
+
+function promptCounty(rl, state, prompt) {
+    return new Promise ((resolve) => {
+        rl.question(prompt, (answer) => checkCounty(answer, state, resolve));
+    });
+};
+
+function checkCounty(answer, state, resolve) {
+    const lowercaseCounty = answer.toLowerCase();
+    const county = state.counties.find(c => c.name.toLowerCase() === lowercaseCounty);
+
+    if(!county) {
+        console.log('Invalid county.');
+        promptCounty(rl, state, 'Try again: ').then(resolve);
+    } else {
+        resolve({stateTax: state.salesTax, countyTax: county.salesTax});
+    };
+};
+
+async function salesTaxCalc() {
+    const order = await promptNumber(rl, 'What is the order amount? ');
+    const stateInfo = await promptState(rl, `What state do you live in? `);
+
+    console.log(stateInfo)
+
+    rl.close();
+};
+
+salesTaxCalc();
