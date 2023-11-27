e259bc323e1e32ede10ae6ec6aa61e63f1e04b27
Danilo <danilomou01@gmail.com>
2023-11-27 14:36:12 -0300

    challenges completed

--- 1/index.js
+++ 1/index.js
@@ -1,10 +1,45 @@
-// for this exercise, it was required to keep input, concatenation and output separate.
+//the book's challenge requires me to create this code without any variables.
 const readline = require('node:readline');
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
 
-const rl = readline.createInterface(process.stdin, process.stdout);
+class Person {
+    constructor(name, greeting) {
+        this.name = name;
+        this.greeting = greeting;
+    };
+};
+
+class People {
+    constructor() {
+        this.peopleArray = []
+    };
+
+    addPerson(person) {
+        this.peopleArray.push(person);
+    };
+
+    findPerson(name) {
+        return this.peopleArray.find(p => p.name === name);
+    }
+}
+
+
+(new People()).addPerson(new Person("John", "Hello, John, nice seeing you!"));
+(new People()).addPerson(new Person("Peter", "Greetings Peter, welcome back to the program."))
+
 
 rl.question('What is your name? ', (answer) => {
-    console.log(`Hello, ${answer}, nice to meet you!`);
+    //idk if this counts as a variable but it looks ugly if I remove it
+    const person = (new People()).find(p => p.name === answer);
+
+    if (person) {
+        console.log(`${person.greeting}`);
+    } else {
+        console.log(`Hello, ${answer}, nice to meet you!`);
+    }
 
     rl.close();
 });
