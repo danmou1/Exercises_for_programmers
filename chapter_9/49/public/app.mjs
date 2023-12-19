a8d4f9950614cd8c6e08eb3af4bb2cca427dcc76
Danilo <danilomou01@gmail.com>
2023-12-19 00:29:33 -0300

    front end wip

--- chapter_9/49/public/app.mjs
+++ chapter_9/49/public/app.mjs
@@ -1,21 +1,63 @@
 // event listeners for the form submission
-document.getElementById('searchForm').addEventListener('submit', async (event) => {
-    event.preventDefault();
 
+const fetchData = async (query) => {
+    try {
+        const response = await fetch(`http://localhost:3000/search?q=${query}`);
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} - ${response.statusText}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error('Error>:', error.message);
+        return [];
+    }
+};
+
+const createImageElement = (imageLink) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = imageLink;
+    imgElement.alt = "Image";
+    return imgElement
+};
+
+const addImagesToContainer = (data, resultsContainer) => {
+    data.forEach((imageLink) => {
+        const imgElement = createImageElement(imageLink);
+
+        const imageContainer = document.createElement('div');
+        imageContainer.classList.add('image-item');
+        imageContainer.appendChild(imgElement);
+
+        resultsContainer.appendChild(imageContainer);
+    });
+};
+
+const displayImages = async () => {
     const searchBox = document.getElementById('searchBox');
     const query = searchBox.value;
 
-    try {
-        const response = await fetch(`http://localhost:3000/search?q=${query}`)
+    const resultsContainer = document.getElementById('resultsContainer');
+    resultsContainer.innerHTML = '';
 
-        if (!response.ok) {
-            throw new Error(`Error: ${response.status} - ${response.statusText}`)
-        }
+    const textContainer = document.createElement('div');
+    
+    const textElement = document.createElement('p');
+    textElement.textContent = `Images about "${query}":`;
+    textElement.classList.add('text-element');
+    textContainer.appendChild(textElement);
+    
+    const lineBreak = document.createElement('br');
+    textContainer.appendChild(lineBreak);
+    
+    const searchButton = document.querySelector('#searchForm button');
+    searchButton.parentNode.insertBefore(textContainer, searchButton.nextSibling);
+    
+    const data = await fetchData(query);
+    addImagesToContainer(data, resultsContainer);
+};
 
-        const data = await response.json();
-
-        console.log(data);
-    } catch (error) {
-        console.error('Error:', error.message);
-    }
+document.getElementById('searchForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    await displayImages();
 });
